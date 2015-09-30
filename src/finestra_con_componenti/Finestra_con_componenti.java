408fe87f3a32ce867b5d8abcd4028dd81213d69f
Lorenzo Carfagna <lorenzo.carfagna@gmail.com>
2015-09-30 15:10:23 +0200

    Prima commit, creato .gitignore

---
+++ src/finestra_con_componenti/Finestra_con_componenti.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finestra_con_componenti;
+import java.awt.*;
+import javax.swing.*;
+/**
+ *
+ * @author Lorenzo
+ */
+public class Finestra_con_componenti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //To do, to be implemented
+    }
+    
+}
